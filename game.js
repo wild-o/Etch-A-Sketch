b412fe0a371400c909d701f42aa591d379b208d7
Albert <alarias1@outlook.com>
2021-12-27 22:27:50 -0600

    Color change upon hovering over elements

--- game.js
+++ game.js
@@ -1,11 +1,41 @@
-
 window.onload = function addDiv(){
     
-    for(let i = 0; i < 6; i++){
-    const newDiv = document.createElement("div");
-    newDiv.className = 'flex-item';
-    
-    const container = document.getElementById('flex');
-    container.appendChild(newDiv);
+    for(let i = 0; i < 64; i++){
+        //Parent - found in the index html 
+        const container = document.getElementById('grid');
+        
+        //Child element
+        const newDiv = document.createElement("div");
+        newDiv.className = 'item';
+        container.appendChild(newDiv);
     }
 }
+
+//Adding an Event Listener to a Dynamically Created Element
+//Event Delegation? 
+function random_bg_color() {
+  
+  let x = Math.floor(Math.random() * 256);
+  let y = Math.floor(Math.random() * 256);
+  let z = Math.floor(Math.random() * 256);
+  let bgColor = "rgb(" + x + "," + y + "," + z + ")";
+
+  console.log(bgColor);
+
+  return bgColor;
+}
+
+function createEventListener() {
+  
+  const myGame = document.getElementById('grid');
+  myGame.addEventListener("mouseover", function(e){
+    const target = e.target; 
+
+    if(target.matches("div")){
+        target.style.backgroundColor = random_bg_color();;
+    }
+  });
+};
+
+createEventListener();
+
