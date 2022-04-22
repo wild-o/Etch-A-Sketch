2708df40c5fa471078c8c6d4ce81d9a1c580b612
Albert <alarias1@outlook.com>
2022-04-21 23:29:33 -0500

    Clear Grid option implemented

--- game.js
+++ game.js
@@ -11,8 +11,29 @@ window.onload = function addDiv(){
     }
 }
 
+// Jan 4th - The next step here is to add event listener to each button element (perhaps look into querySelector all? 
+// refer here: https://stackoverflow.com/questions/51573435/want-to-add-addeventlistener-on-multiple-elements-with-same-class
+
+
+// Jan 6th attempting to use Event Delegation in order to select buttons from the outer node. 
+
+
 //Adding an Event Listener to a Dynamically Created Element
 //Event Delegation? 
+
+function clearGrid(){
+  const myButt = document.getElementById("1")
+  
+  myButt.addEventListener("click", function(e){
+    console.log(e);
+    const testingClear = document.querySelectorAll('.item')
+    
+    testingClear.forEach(item => {
+      item.style.backgroundColor = "#FFFFFF";
+    })
+  })
+}
+
 function random_bg_color() {
   
   let x = Math.floor(Math.random() * 256);
@@ -25,17 +46,31 @@ function random_bg_color() {
   return bgColor;
 }
 
-function createEventListener() {
+function RainbowMode() {
   
   const myGame = document.getElementById('grid');
   myGame.addEventListener("mouseover", function(e){
     const target = e.target; 
 
     if(target.matches(".item")){
-        target.style.backgroundColor = random_bg_color();;
+        target.style.backgroundColor = random_bg_color();
     }
   });
 };
 
-createEventListener();
+function buttons(){
+  const myButt = document.getElementById("1")
+ 
+ myButt.addEventListener("click", function(e){
+   console.log(e);
+   const clearGrid = document.getElementsByClassName('item');
+   clearGrid.style.backgroundColor = random_bg_color(); 
+   })
+};
 
+
+
+
+
+RainbowMode();
+clearGrid();
